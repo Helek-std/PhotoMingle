04f429133efcc7b5df6184cf9edf6a2dc9a58437
Sh1Fu <shifu.work@gmail.com>
2025-04-21 03:00:44 +0300

    [prod] fix .env template, fix python code to 3.8 version, add production deps versions

--- Photomingle/Photomingle/settings.py
+++ Photomingle/Photomingle/settings.py
@@ -159,8 +159,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.parent / 'static'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
